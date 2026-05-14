764e1624279156c04715241f351c088d5262187a
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-05-14 15:00:03 +0700

    menambahkan implementasi metode push pada kelas stack

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -32,4 +32,8 @@ public:
         newNode->data = value;
         newNode->next = top;
         top = newNode;
-        
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    
